5512e89ea71634e09cfcc6579666bdf5792fe08a
Sonal <sonal@magicbricks.com>
2024-06-08 00:05:17 +0530

    Initial Commit

---
+++ tut1/tut1App.swift
@@ -0,0 +1,17 @@
+//
+//  tut1App.swift
+//  tut1
+//
+//  Created by Sonal on 08/06/24.
+//
+
+import SwiftUI
+
+@main
+struct tut1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
